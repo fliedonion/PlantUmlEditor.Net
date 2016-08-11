d16c43ba0e985135479bdc4c6eb17613054b0bfb
Takahiro KITAHARA <fliedonion@users.noreply.github.com>
2016-08-12 00:40:10 +0900

    Add Export Emf with Inkscape feature

--- client/CaseOfT.Net.PlantUMLClient/CaseOfT.Net.PlantUMLClient/Pages/EditorPage.xaml.cs
+++ client/CaseOfT.Net.PlantUMLClient/CaseOfT.Net.PlantUMLClient/Pages/EditorPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reactive.Linq;
@@ -45,5 +47,87 @@ namespace CaseOfT.Net.PlantUMLClient {
                 });
 
         }
+
+        private void exportEmfButton_Click(object sender, RoutedEventArgs e) {
+            try {
+                var result = exportEmf();
+
+                if (result != null && result == false) {
+                    MessageBox.Show("出力に失敗しました。", "Warning", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                }
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Export Error \r\n" + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private bool? exportEmf() {
+
+            var dlg = new SaveFileDialog();
+            dlg.FilterIndex = 1;
+            dlg.Title = "EMFファイルにエクスポートします";
+            dlg.Filter = "EMF (*.emf)|*.emf|All Files(*.*)|*.*";
+            bool? result = dlg.ShowDialog();
+            if (result != null && result == true) {
+                return SaveToSvg(dlg.FileName);
+            }
+            return result;
+        }
+
+        private bool SaveToSvg(string saveFilename) {
+            try {
+                var tcp = new Tcp.PlantUmlTcpClient();
+                var svg = tcp.RenderRequest(sourceEditor.Text);
+                return ExportToEmf(svg, saveFilename);
+            }
+            catch (Exception ex) {
+                throw new Exception("ローカルレンダリングサーバーと通信できませんでした。\r\n" + ex.Message, ex);
+            }
+        }
+
+        private bool ExportToEmf(string svg, string saveFilename) {
+            try {
+                var tmpFile = System.IO.Path.GetTempFileName();
+                File.WriteAllText(tmpFile, svg);
+                var result = CallLinkScape(tmpFile, saveFilename);
+
+                if (result.Item1 != 0) {
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception ex) {
+                throw new Exception("一時ファイル作成に失敗しました。\r\n" + ex.Message, ex);
+            }
+        }
+
+        private const string InkScapePath = @"c:\Program Files\Inkscape\inkscape.exe";
+        private const string InkScapeArgFormat = @" ""{0}"" --export-emf=""{1}""";
+
+        private Tuple<int, string, string> CallLinkScape(string tempSvgFile, string saveFilename) {
+
+            try {
+                string args = string.Format(InkScapeArgFormat, tempSvgFile, saveFilename);
+
+                var pinfo = new ProcessStartInfo(InkScapePath, args);
+                pinfo.UseShellExecute = false;
+                pinfo.CreateNoWindow = false;
+                pinfo.RedirectStandardOutput = true;
+                pinfo.RedirectStandardError = true;
+
+                var p = Process.Start(pinfo);
+                var stdout = p.StandardOutput.ReadToEnd();
+                var stderr = p.StandardError.ReadToEnd();
+                p.WaitForExit();
+
+                return Tuple.Create(p.ExitCode, stdout, stderr);
+
+            }
+            catch (Exception ex) {
+                throw new Exception("inkscapeの起動で例外が発生しました。\r\n" + ex.Message, ex);
+            }
+        }
+
     }
 }
